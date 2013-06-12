f468cc1e3702b71adb262ac428289f302687ba17
Attila Szeremi <attila.szeremi@netefficiency.co.uk>
2013-06-12 13:06:52 +0100

    Env bootstrap

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -14,8 +14,44 @@ use Zend\Mvc\MvcEvent;
 
 class Module
 {
+    public $isAttila = false; // Server is Attila's local server
+    public $isLocalDev = false; // Server is a local dev server
+    public $levelStaging = false; // APPLICATION_ENV is at least staging level
+    public $levelDevelopment = false; // APPLICATION_ENV is at least development level
+    public $isDevIp = false;
+    public $isDebugOutputSafe = false; // Is it safe to output debug information
+    public $isDebugInvisibleSafe = false; // Is it safe to invisibly output debug information
+    public $showAnalytics = false;
+
+    protected function _initEnv() {
+        if (!defined('APPLICATION_ENV')) {
+            $appEnv = getenv('APPLICATION_ENV');
+            if (!$appEnv) {
+                $appEnv = 'production';
+            }
+            define('APPLICATION_ENV', $appEnv);
+        }
+        $appEnv = APPLICATION_ENV;
+
+        switch (APPLICATION_ENV) {
+        case 'testing':
+        case 'development':
+            $this->levelDevelopment = true;
+        case 'staging':
+            $this->levelStaging = true;
+        case 'production':
+            break;
+        }
+        if ('development' == APPLICATION_ENV) {
+            $this->levelStaging = true;
+        }
+        $this->isDebugOutputSafe = $this->isLocalDev || $this->isDevIp;
+        $this->isDebugInvisibleSafe = $this->isDebugOutputSafe || $this->isLocalDev || $this->levelStaging;
+    }
+
     public function onBootstrap(MvcEvent $e)
     {
+        $this->_initEnv();
         $e->getApplication()->getServiceManager()->get('translator');
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();

3bb760adcc9ab8895fcf7ef59e5d39e07c288868
Attila Szeremi <attila.szeremi@netefficiency.co.uk>
2013-06-19 16:04:54 +0100

    Fix: cache config array was in the wrong format

--- module/Lycee/config/module.config.php
+++ module/Lycee/config/module.config.php
@@ -3,11 +3,15 @@ return array (
     'caches' => array (
         // doesn't have to look FQNS, but I think I heard it's good practise
         'Lycee\Cache' => array (
-            'adapter' => 'filesystem',
-            'ttl' => 88400,
-            'cache_dir' => './data/cache/lycee',
-            'dir_permission' => 0777,
-            'file_permission' => 0666,
+            'adapter' => array (
+                'name' => 'filesystem',
+                'options' => array (
+                    'ttl' => 88400,
+                    'cache_dir' => './data/cache/lycee',
+                    'dir_permission' => 0777,
+                    'file_permission' => 0666,
+                )
+            )
         )
     )
 );

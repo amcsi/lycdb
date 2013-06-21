bda4d94d443681fd09ae530114811558679ee1e6
amcsi <amcsi@adwin>
2013-06-21 23:56:14 +0100

    Search logic written in model

--- module/Lycee/src/Lycee/Model.php
+++ module/Lycee/src/Lycee/Model.php
@@ -14,6 +14,8 @@ class Model {
     }
 
     public function get($options = array ()) {
+        $amysql = $this->amysql;
+
         $limit = ' LIMIT 50 ';
         $offset = ' OFFSET 0 ';
         $wheres = array ();
@@ -22,6 +24,100 @@ class Model {
         $binds['ex'] = 2;
         $where = $wheres ? ' WHERE ' . join (' AND ', $wheres) : '';
 
+        if (!empty($options['name'])) {
+            $expr = $amysql->expr(\AMysql_Expr::ESCAPE_LIKE, $options['name']);
+            $wheres[] = 'name_jp LIKE :name OR name_en LIKE :name';
+            $binds['name'] = $options['name'];
+        }
+
+        if (isset($options['type']) && -2 < ($type = $options['type'])) {
+            /**
+             * Not a character 
+             */
+            if (-1 == $type) {
+                $wheres[] = "type != :type";
+                $binds['type'] = Card::CHAR;
+            }
+            else {
+                $wheres[] = "type = :type";
+                $binds['type'] = $type;
+            }
+        }
+        $elements = array ('snow', 'moon', 'flower', 'lightning', 'sun', 'star');
+        if (!empty($options['cost'])) {
+            $costType = isset($options['cost_type']) ? $options['cost_type'] : 1;
+            // exact cost
+            if (2 == $costType) {
+                foreach ($elements as $key => $element) {
+                    $wheres[] = "cost_$element = :cost_$element";
+                    $binds["cost_$element"] = isset($cost[$key]) ? $cost[$key] : 0;
+                }
+            }
+            // payable by
+            else {
+                $total = 0;
+                $starWheres = array ();
+                foreach ($elements as $key => $element) {
+                    $costAmount = isset($options['cost'][$key]) ? $options['cost'][$key] : 0;
+                    $total += $costAmount;
+                    $starWheres[] = "cost_$element";
+                    if ($key != Lycee::STAR) {
+                        $wheres[] = "cost_$element <= :cost_$element";
+                        $binds["cost_$element"] = $costAmount;
+                    }
+                    else {
+                        $wheres[] = join (' + ', $starWheres) . ' <= :cost_total';
+                        $binds['cost_total'] = $total;
+                    }
+                }
+            }
+        }
+
+        if (isset($options['ex'], $options['ex_equality'])) {
+            $eq = $options['ex_equality'];
+            $op = '=';
+            if (0 < $eq) {
+                $op = '<=';
+            }
+            else if ($eq < 0) {
+                $op = '>=';
+            }
+            $wheres[] = "ex $op :ex";
+            $binds['ex'] = $options['ex'];
+        }
+
+        if (isset($options['element'])) {
+            $elementType = isset($options['element_type']) ? $options['element_type'] : 1;
+            // is
+            if (2 == $elementType) {
+                foreach ($elements as $key => $element) {
+                    if (Lycee::STAR == $key) {
+                        continue;
+                    }
+                    $op = !empty($options['element'][$key]) ? '!=' : '=';
+                    $wheres[] = "is_$element $op 0";
+                }
+            }
+            // has
+            else {
+                foreach ($elements as $key => $element) {
+                    if (Lycee::STAR == $key) {
+                        continue;
+                    }
+                    if (!empty($options['element'][$key])) {
+                        $wheres[] = "is_$element != 0";
+                    }
+                }
+            }
+        }
+
+        if (!empty($options['text'])) {
+            $wheres[] = 'ability_desc_jp LIKE :text OR ability_desc_en LIKE :name
+                OR comments_jp LIKE :text OR comments_en LIKE :text
+            ';
+            $expr = $amysql->expr(\AMysql_Expr::ESCAPE_LIKE, $options['text']);
+            $binds['text'] = $expr;
+        }
 
         $sql = "SELECT SQL_CALC_FOUND_ROWS * FROM $this->cardsTableName $where $limit $offset";
         $stmt = $this->amysql->prepare($sql);
